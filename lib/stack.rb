14f9fa6e93b2fc7231d578d14611f5c42861bf57
github username <github email address>
2017-04-30 15:16:49 -0400

    Done.

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,20 @@
 class Stack
 
+  def initialize
+    @elements = []
+  end
+
+  def push(data)
+    @elements.push(data)
+    self
+  end
+
+  def pop
+    @elements.pop
+  end
+
+  def size
+    @elements.count
+  end
+
 end
