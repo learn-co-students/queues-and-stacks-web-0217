57b30613990866f1e0467de655130cdc447d01a5
hwalborn <hwalborn@gmail.com>
2017-05-08 10:21:27 -0400

    now I want a push-pop

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,24 @@
 require_relative 'stack'
 
 class Queue
+  def initialize
+    @stack1 = Stack.new
+    @stack2 = Stack.new
+  end
+
+  def enqueue(item)
+    @stack1.push(item)
+  end
+
+  def dequeue
+    while @stack1.size > 0
+      @stack2.push(@stack1.pop)
+    end
+    pop_return = @stack2.pop
+    while @stack2.size > 0
+      @stack1.push(@stack2.pop)
+    end
+    pop_return
+  end
 
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,18 @@
 class Stack
+  attr_accessor :arr
+  def initialize
+    @arr = []
+  end
 
+  def push(item)
+    @arr << item
+  end
+
+  def pop
+    @arr.pop
+  end
+
+  def size
+    @arr.length
+  end
 end
