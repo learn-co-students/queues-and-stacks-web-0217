00ad8e4af30f8c7f2b90a797df579b205be83fe3
jef4490 <jeffrey.hechler@gmail.com>
2017-05-09 10:26:22 -0400

    Done.

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,19 @@
 class Stack
+  attr_accessor :stack
+  def initialize
+    @stack = []
+  end
+
+  def push(element)
+    stack.push(element)
+  end
+
+  def pop
+    stack.pop
+  end
+
+  def size
+    stack.length
+  end
 
 end
