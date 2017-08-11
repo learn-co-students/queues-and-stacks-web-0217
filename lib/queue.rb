6b55127266e52c5710ee74414ca1ab45fe0b1d97
Rathi <github email address>
2017-08-11 10:04:06 -0400

    did this lab

--- lib/queue.rb
+++ lib/queue.rb
@@ -2,4 +2,22 @@ require_relative 'stack'
 
 class Queue
 
+  def initialize
+    @enq_elements = Stack.new
+    @deq_elements = Stack.new
+  end
+
+  def enqueue(val)
+   @enq_elements.push val
+  end
+
+  def dequeue
+    if @deq_elements.size == 0
+      while @enq_elements.size > 0
+        @deq_elements.push @enq_elements.pop
+      end
+    end
+    @deq_elements.pop
+  end
+
 end
