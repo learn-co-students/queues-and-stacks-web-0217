66b0fccf4a4fd1caef04cd0c4c7a6489e0c58436
Rathi <github email address>
2017-08-11 09:53:15 -0400

    made stack class

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,21 @@
 class Stack
 
+  def initialize
+    @elements = []
+  end
+
+  def push(element)
+    @elements.push(element)
+    self
+  end
+
+  def pop
+    @elements.pop
+  end
+
+  def size
+    @elements.length
+    # @elements.size
+  end
+
 end
